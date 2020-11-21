6c379253f52cb537b7b0898fca57aac42afc7a83
Emamnuel <emmanuel@teepublic.com>
2020-11-21 13:09:09 -0500

    Update tests to parameterized

--- spec/point_of_sale_spec.rb
+++ spec/point_of_sale_spec.rb
@@ -5,20 +5,19 @@ RSpec.describe PointOfSale do
   describe 'getting the right price' do
     barcode_to_price = {
       "12345" => "$123.45",
+      "12345\n" => "$123.45",
+      "12345\r" => "$123.45",
       "23456" => "$234.56",
     }
-    it 'returns the right price for a barcode' do
-      pos = PointOfSale.new
-      pos.barcode_scan("12345\n")
 
-      expect(pos.price).to eq(barcode_to_price["12345"])
+    barcode_to_price.each do |scan, output|
+      it "output is #{output} when given #{scan}" do
+        pos = PointOfSale.new
+        pos.barcode_scan(scan)
+
+        expect(pos.price).to eq(output)
+      end
     end
 
-    it 'returns the right price for 23456' do
-      pos = PointOfSale.new
-      pos.barcode_scan("23456\n")
-
-      expect(pos.price).to eq(barcode_to_price["23456"])
-    end
   end
 end
