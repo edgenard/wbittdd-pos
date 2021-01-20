0d6c30c31d689b24aeba0c78d13cc3eb468abb1e
Emamnuel <emmanuel@teepublic.com>
2021-01-20 09:58:39 -0500

    Reset to nothing write up new constraints

--- lib/point_of_sale.rb
+++ lib/point_of_sale.rb
@@ -1,24 +1,2 @@
 class PointOfSale
-  BARCODE_TO_PRICE = {
-    "12345" => "$123.45",
-    "23456" => "$234.56",
-    "34567" => "$678.90",
-    "Not Found" => "Not Found",
-    "Wrong#" => "Invalid Input must be exactly five digits"
-  }.freeze
-  def barcode_scan(input)
-    @input = sanitize_input(input)
-  end
-
-  def price
-    BARCODE_TO_PRICE[@input]
-  end
-
-  def sanitize_input(input)
-    stripped_input = input.strip
-    return "Wrong#" unless stripped_input.match?(/^\d{5}$/)
-    return "Not Found" if BARCODE_TO_PRICE[stripped_input].nil?
-
-    stripped_input
-  end
 end
--- spec/point_of_sale_spec.rb
+++ spec/point_of_sale_spec.rb
@@ -2,24 +2,4 @@ require "spec_helper"
 require_relative "../lib/point_of_sale"
 
 RSpec.describe PointOfSale do
-  describe "getting the right price" do
-    barcode_to_price = {
-      "12345" => "$123.45",
-      "23456\n" => "$234.56",
-      "34567\r" => "$678.90",
-      "\n" => "Invalid Input must be exactly five digits",
-      "45678" => "Not Found",
-      "123\n" => "Invalid Input must be exactly five digits",
-      "567890" => "Invalid Input must be exactly five digits"
-    }
-
-    barcode_to_price.each do |scan, output|
-      it "output is #{output} when given #{scan}" do
-        pos = PointOfSale.new
-        pos.barcode_scan(scan)
-
-        expect(pos.price).to eq(output)
-      end
-    end
-  end
 end
