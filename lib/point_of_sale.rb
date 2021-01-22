31548d2a83a6f8807f09e4950c7f60f6a35e5ea4
Emamnuel <emmanuel@teepublic.com>
2021-01-22 10:52:37 -0500

    Raise Error if price is not found with given barcode

--- lib/point_of_sale.rb
+++ lib/point_of_sale.rb
@@ -1,11 +1,14 @@
 class PointOfSale
-
   def initialize(price_map)
     @price_map = price_map
   end
 
   def price(barcode)
+    raise PriceNotFoundError unless @price_map[barcode]
     @price_map[barcode]
+  end
+
+  class PriceNotFoundError < StandardError
   end
 end
 
--- spec/point_of_sale_spec.rb
+++ spec/point_of_sale_spec.rb
@@ -2,15 +2,25 @@ require "spec_helper"
 require_relative "../lib/point_of_sale"
 
 RSpec.describe PointOfSale do
-  describe 'price' do
-    context 'when barcode matches with price' do
-      it 'returns price' do
+  describe "price" do
+    context "when barcode matches with price" do
+      it "returns price" do
         barcode = Barcode.new
-        price_map = Hash.new
+        price_map = {}
         price_map[barcode] = 123.45
         pos = PointOfSale.new(price_map)
 
         expect(pos.price(barcode)).to eq 123.45
+      end
+    end
+
+    context "when barcode does not match a price" do
+      it "raises an error" do
+        barcode = Barcode.new
+        price_map = {}
+        pos = PointOfSale.new(price_map)
+
+        expect { pos.price(barcode) }.to raise_error(PointOfSale::PriceNotFoundError)
       end
     end
   end
